0829fdc99d5ba0a24e4cb513036a1e237e221839
Ganesha Maruthi <ultimatechanti@gmail.com>
2022-08-19 19:08:00 +0530

    Added Database support

---
+++ db.py
@@ -0,0 +1,8 @@
+import sqlalchemy
+
+engine = sqlalchemy.create_engine('postgresql://postgres:postgres@localhost:5432/sspdim')
+metadata = sqlalchemy.MetaData()
+
+connection = engine.connect()
+
+userinfo = sqlalchemy.Table('userinfo', metadata, autoload_with = engine)
--- server.py
+++ server.py
@@ -1,26 +1,48 @@
 from flask import Flask, request, jsonify
+from db import userinfo, connection
+import sqlalchemy as db
 
 app = Flask(__name__)
 
 @app.route('/login', methods=['POST'])
 def login():
-    if(request.headers.get('Content-Type') == 'application/json'):
-        res = {
-            "username" : request.json["username"],
-            "password" : request.json["password"],
-            "response-code": 200
-        }
-        return jsonify(res)
+    query = db.select([userinfo]).where(userinfo.columns.username == request.json['username'])
+    res = connection.execute(query)
+    result = res.fetchall()
+    if result and result[0][1] == request.json['password']:
+        return jsonify({
+            'status': 200,
+            'message': 'Found user'
+        })
+    else:
+        return jsonify({
+            'status': 400,
+            'message': 'Did not find user'
+        })
 
 @app.route('/register', methods=['POST'])
 def register():
-    if(request.headers.get('Content-Type') == 'application/json'):
-        res = {
-            "username" : request.json["username"],
-            "password" : request.json["password"],
-            "response-code": 404
-        }
-        return jsonify(res)
+    query = db.select([userinfo]).where(userinfo.columns.username == request.json['username'])
+    res = connection.execute(query)
+    result = res.fetchall()
+    if result:
+        return jsonify({
+            'status': 400,
+            'message': 'Username already taken'
+        })
+    else:
+        query = db.insert(userinfo).values(username = request.json['username'], password = request.json['password'])
+        try:
+            res = connection.execute(query)
+            return jsonify({
+                'status': 200,
+                'message': 'Registration successful'
+            })
+        except:
+            return jsonify({
+                'status': 400,
+                'message': 'Registration unsuccessful'
+            })
 
 if __name__ == '__main__':
     app.run(debug = True)

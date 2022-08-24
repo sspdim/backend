e193e7a9a76916b13b05db3859d9173a0d861002
Dev <devvmashru@protonmail.ch>
2022-08-24 21:42:22 +0530

    Add root route

--- server.py
+++ server.py
@@ -47,5 +47,9 @@ def register():
                 'message': 'Registration unsuccessful'
             })
 
+@app.route('/', methods=['GET'])
+def home():
+    return "Hello World"
+
 if __name__ == '__main__':
     app.run(debug = True)

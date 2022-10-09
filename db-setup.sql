ab06f91ee4c2dea274e394efe838e723a8950efd
Ganesha Maruthi <ultimatechanti@gmail.com>
2022-10-09 12:07:36 +0530

    Added tables for pending messages

--- db-setup.sql
+++ db-setup.sql
@@ -3,7 +3,11 @@ create database sspdim;
 \c sspdim;
 drop table userinfo;
 drop table servers;
+
 create table userinfo(username varchar primary key, password varchar(256));
 create table servers(ip_address varchar primary key, domain_name varchar, status varchar);
 create table tokens(token varchar primary key, username varchar);
+create table pending_friend_requests(from_username varchar, to_username varchar, primary key (from_username, to_username));
+create table pending_messages(from_username varchar, to_username varchar, message_content varchar, message_id varchar, primary key (from_username, to_username, message_id));
+
 insert into servers values('35.209.49.77', 'none', 'active');
